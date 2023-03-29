8dc5cf0d3bc5c0a18ce1f94d5e3f54af6ac835d6
MarufAlAslam <55714750+MarufAlAslam@users.noreply.github.com>
2023-03-29 17:27:32 +0600

    featured slider done

--- js/app.js
+++ js/app.js
@@ -3,3 +3,38 @@ $('.filter-btn').click(function(e){
     $('.filter-btn').removeClass('active')
     $(this).addClass('active')
 })
+
+
+// featured slider
+$(".featured-slider").slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
