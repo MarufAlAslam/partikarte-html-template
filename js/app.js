cc8c371e418cb2f64a955c333c22b4f251cd85ec
MarufAlAslam <55714750+MarufAlAslam@users.noreply.github.com>
2023-03-29 19:21:49 +0600

    explore slider done

--- js/app.js
+++ js/app.js
@@ -13,9 +13,46 @@ $(".featured-slider").slick({
   slidesToShow: 4,
   slidesToScroll: 1,
   arrows: true,
-  prevArrow:
-    '<button class="btn btn-prev"><img src="img/f-prev.png" class="img-fluid" alt=""></button>',
-    nextArrow: '<button class="btn btn-next"><img src="img/f-prev.png" class="img-fluid" alt=""></button>',
+  prevArrow: $('.btn-prev'),
+  nextArrow: $('.btn-next'),
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
+
+
+// featured slider
+$(".bottom-slider").slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  arrows: true,
+  prevArrow: $('.b-prev'),
+  nextArrow: $('.b-next'),
   responsive: [
     {
       breakpoint: 1024,
